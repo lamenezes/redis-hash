c25604533d246131e2bc36f9303bf663c73e6e1f
Luiz Menezes <luiz.menezesf@gmail.com>
2018-03-02 00:23:26 -0300

    Use redis client methods for length and contains magic methods

--- tests/test_hash.py
+++ tests/test_hash.py
@@ -12,13 +12,17 @@ class FakeRedisClient:
         self.connection_pool = mock.Mock(connection_kwargs=kwargs)
 
     def hget(self, hash_name, key):
-        return self._store.get(key).encode('utf-8')
+        return self._store[key].encode('utf-8')
 
     def hset(self, hash_name, key, value):
         self._store[key] = value
 
     def hdel(self, hash_name, key):
-        del self._store[key]
+        try:
+            del self._store[key]
+            return 1
+        except KeyError:
+            return 0
 
     def hscan_iter(self, hash_name):
         for k, v in self._store.items():
@@ -31,6 +35,11 @@ class FakeRedisClient:
 @pytest.fixture
 def redis_hash():
     return RedisHash(FakeRedisClient(), 'hashish')
+
+
+@pytest.fixture
+def mock_redis_hash():
+    return RedisHash(mock.MagicMock(), 'hashish')
 
 
 def test_redis_hash(redis_hash):
@@ -54,3 +63,26 @@ def test_redis_hash(redis_hash):
     assert len(redis_hash) == 1
     del redis_hash['foo']
     assert len(redis_hash) == 0
+
+
+def test_redis_hash_getitem_keyerror(redis_hash):
+    with pytest.raises(KeyError):
+        redis_hash['nx']
+
+
+def test_redis_hash_delitem_keyerror(redis_hash):
+    with pytest.raises(KeyError):
+        del redis_hash['nx']
+
+
+def test_redis_hash_contains(mock_redis_hash):
+    key = 'eita'
+    assert key in mock_redis_hash
+
+    mock_redis_hash._client.hexists.assert_called_with(mock_redis_hash._hash_name, key)
+
+
+def test_redis_hash_len(mock_redis_hash):
+    assert len(mock_redis_hash)
+
+    mock_redis_hash._client.hlen.assert_called_with(mock_redis_hash._hash_name)
